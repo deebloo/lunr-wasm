8780e371f55e77344c9f30972ba186a5bef47bf4
dblue <dblue@deloitte.com>
2021-03-29 13:53:33 -0400

    update example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+mod index;
+
+use index::Index;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -7,4 +10,14 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub struct Lunr {}
+pub struct Lunr {
+    index: Index,
+}
+
+impl Lunr {
+    pub fn add_document(&mut self) {
+        self.index.docs.push(String::from("Hello World"))
+    }
+
+    pub fn search() {}
+}
